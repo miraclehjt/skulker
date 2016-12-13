385f0ff297fb5c686466b2f0a7f0816c00715f0d
lpw <heisedebaise@hotmail.com>
2016-12-13 14:38:56 +0800

    修改Ranch生成配置。

--- src/main/java/org/lpw/skulker/Ranch.java
+++ src/main/java/org/lpw/skulker/Ranch.java
@@ -12,6 +12,19 @@ public class Ranch {
         comment();
     }
 
+    private static void doc() {
+        copy("Doc", null, new String[][]{{"c_key", "VARCHAR(255)", "k", "类型KEY"},
+                {"c_owner", "FK", "k", "所有者ID"}, {"c_author", "FK", "k", "作者ID"},
+                {"c_score_min", "INT", "", "最小分值"}, {"c_score_max", "INT", "", "最大分值"},
+                {"c_sort", "INT", "", "顺序"}, {"c_subject", "VARCHAR(255)", "n", "标题"},
+                {"c_image", "VARCHAR(255)", "", "主图URI地址"}, {"c_thumbnail", "VARCHAR(255)", "", "缩略图URI地址"},
+                {"c_summary", "TEXT", "", "摘要"}, {"c_label", "TEXT", "", "标签"},
+                {"c_content", "TEXT", "n", "内容"}, {"c_read", "INT", "", "阅读次数"},
+                {"c_favorite", "INT", "", "收藏次数"}, {"c_comment", "INT", "", "评论次数"},
+                {"c_score", "INT", "", "得分"}, {"c_time", "DATETIME", "n", "时间"}
+        }, false, true);
+    }
+
     private static void comment() {
         copy("Comment", null, new String[][]{{"c_key", "VARCHAR(255)", "", "服务KEY"},
                 {"c_owner", "FK", "k", "所有者ID"}, {"c_author", "FK", "k", "作者ID"},
@@ -36,9 +49,10 @@ public class Ranch {
                 Module.parse("lpw", module, (pkg == null ? "org.lpw.ranch" : ("org.lpw.ranch." + pkg)), null,
                         "org.lpw.ranch.recycle", "RecycleModelSupport", 36, array);
             } else if (audit) {
-                String[][] array = new String[columns.length + 1][];
+                String[][] array = new String[columns.length + 2][];
                 System.arraycopy(columns, 0, array, 0, columns.length);
-                array[array.length - 1] = new String[]{"c_audit", "INT", "ignore", "审核：0-待审核；1-审核通过；2-审核不通过"};
+                array[array.length - 2] = new String[]{"c_audit", "INT", "ignore", "审核：0-待审核；1-审核通过；2-审核不通过"};
+                array[array.length - 1] = new String[]{"c_audit_remark", "VARCHAR(255)", "ignore", "审核备注"};
 
                 Module.parse("lpw", module, (pkg == null ? "org.lpw.ranch" : ("org.lpw.ranch." + pkg)), null,
                         "org.lpw.ranch.audit", "AuditModelSupport", 36, array);
