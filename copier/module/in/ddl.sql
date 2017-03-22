47dab9349a45aba08bbf9104e5cad1ba8e12783e
lpw <heisedebaise@hotmail.com>
2017-03-22 17:14:06 +0800

    修改Ranch。 修改DDL模板。

--- copier/module/in/ddl.sql
+++ copier/module/in/ddl.sql
@@ -13,11 +13,11 @@ CREATE TABLE t${prefix}_${module_name}
 <#if columns?? && (columns?size>0)>
 <#list columns as column>
   ${column.name} ${column.type?upper_case} <#if column.number && !column.auto>DEFAULT 0<#else><#if column.notNull>NOT<#else>DEFAULT</#if> NULL</#if> COMMENT '${column.comment}',
-  <#if column.unique><#assign keys=keys+",\n  UNIQUE KEY uk"+prefix+"_"+module_name+"_"+column.name?remove_beginning("c_")+"("+column.name+")"/></#if>
-  <#if column.key><#assign keys=keys+",\n  KEY k"+prefix+"_"+module_name+"_"+column.name?remove_beginning("c_")+"("+column.name+") USING HASH"/></#if>
+  <#if column.unique><#assign keys=keys+",\n  UNIQUE KEY uk_"+column.name?remove_beginning("c_")+"("+column.name+")"/></#if>
+  <#if column.key><#assign keys=keys+",\n  KEY k_"+column.name?remove_beginning("c_")+"("+column.name+") USING HASH"/></#if>
   <#if column.auto><#assign auto=true/></#if>
 </#list>
 </#if>
 
-  PRIMARY KEY pk${prefix}_${module_name}(c_id) USING HASH${keys}
+  PRIMARY KEY pk(c_id) USING HASH${keys}
 ) ENGINE=InnoDB<#if auto> AUTO_INCREMENT=1</#if> DEFAULT CHARSET=utf8;
